283d2212e388c57d5c9238665ca41ade6a92975d
mrizkir <cvyacanet@gmail.com>
2019-05-15 10:17:28 +0700

    perbaiki redirect di web

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 //disable halaman register [pendaftaran]
 Auth::routes(['register' => false]);
 
-Route::get('/',function (){    
-    if (!Auth::check() && !Request::is('login')) {
-        return view('welcome');
-    }else{
-        return Redirect::route('dashboard.index');
-    }
-}) ;
+Route::get('/',['uses'=>'FrontendController@welcome','as'=>'frontend.index']);
 Route::get('/logout',['uses'=>'Auth\LoginController@logout','as'=>'logout']);
 Route::group (['prefix'=>'admin','middleware'=>['disablepreventback','web', 'auth']],function() {     
     Route::get('/',['uses'=>'DashboardController@index','as'=>'dashboard.index']);          
