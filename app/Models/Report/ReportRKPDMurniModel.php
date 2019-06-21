aaa1b3f0440e0a010dd27985b6e5e1657af334fd
mrizkir <cv.yacanet@nanhekumar.com>
2019-06-21 10:27:25 +0700

    perbaikan print rkpd 1

--- app/Models/Report/ReportRKPDMurniModel.php
+++ app/Models/Report/ReportRKPDMurniModel.php
@@ -3,6 +3,9 @@
 namespace App\Models\Report;
 
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Common\Entity\Style\Color;
+
 use App\Models\RKPD\RKPDMurniModel;
 
 class ReportRKPDMurniModel extends ReportModel
@@ -17,30 +20,19 @@ class ReportRKPDMurniModel extends ReportModel
     private function print($filename)  
     {
         $writer=$this->writer;
-        $this->customTempFolderPath.DIRECTORY_SEPARATOR.$filename;
+        //default style
+        $writer->setDefaultRowStyle((new StyleBuilder())
+                                    ->setFontSize(8)
+                                    ->build());
+
         $writer->openToFile($this->customTempFolderPath.DIRECTORY_SEPARATOR.$filename);
 
-        // $cells = [
-        //     WriterEntityFactory::createCell('Carl'),
-        //     WriterEntityFactory::createCell('is'),
-        //     WriterEntityFactory::createCell('great!'),
-        // ];
-        
-        // // /** add a row at a time */
-        // $singleRow = WriterEntityFactory::createRow($cells);
-        // $writer->addRow($singleRow);
-
-        // /** add multiple rows at a time */
-        // $multipleRows = [
-        //     WriterEntityFactory::createRow($cells),
-        //     WriterEntityFactory::createRow($cells),
-        // ];
-        // $writer->addRows($multipleRows); 
         $OrgID = $this->dataReport['OrgID'];
         
         $daftar_program=RKPDMurniModel::select(\DB::raw('"PrgID","kode_program","PrgNm"'))
                                         ->where('OrgID',$OrgID)
                                         ->where('TA',\HelperKegiatan::getTahunPerencanaan())
+                                        ->orderBy('kode_program','ASC')
                                         ->get()->toArray();
 
         foreach ($daftar_program as $v)
@@ -48,7 +40,8 @@ class ReportRKPDMurniModel extends ReportModel
             $PrgID=$v['PrgID'];
 
             $daftar_kegiatan = RKPDMurniModel::select(\DB::raw('"kode_kegiatan","KgtNm","NamaIndikator","Sasaran_Angka1","Sasaran_Uraian1","Target1","NilaiUsulan1","Sasaran_AngkaSetelah","Sasaran_UraianSetelah","NilaiSetelah","Nm_SumberDana","Descr"'))
-                                            ->where('PrgID',$PrgID)             
+                                            ->where('PrgID',$PrgID)      
+                                            ->orderBy('kode_kegiatan','ASC')       
                                             ->get();
             $totalpagueachprogram= $daftar_kegiatan->sum('NilaiUsulan1');      
             $cells = [
@@ -57,12 +50,22 @@ class ReportRKPDMurniModel extends ReportModel
                 WriterEntityFactory::createCell(''),
                 WriterEntityFactory::createCell(''),
                 WriterEntityFactory::createCell(''),
-                WriterEntityFactory::createCell($totalpagueachprogram),
+                WriterEntityFactory::createCell(\Helper::formatAngka($totalpagueachprogram)),
             ];
         
             // /** add a row at a time */
-            $singleRow = WriterEntityFactory::createRow($cells);
-            $writer->addRow($singleRow);
+            /** Create a style with the StyleBuilder */
+            $style_program = (new StyleBuilder())
+                    ->setFontBold()
+                    ->setFontSize(10)
+                    ->setFontColor(Color::BLUE)
+                    ->setShouldWrapText()
+                    ->setBackgroundColor(Color::YELLOW)
+                    ->build();
+            $row = WriterEntityFactory::createRow($cells);
+            $row->setStyle($style_program);
+            $writer->addRow($row);
+
             $multipleRows=[];
             foreach ($daftar_kegiatan as $n) 
             {
@@ -72,11 +75,9 @@ class ReportRKPDMurniModel extends ReportModel
                     WriterEntityFactory::createCell($n['NamaIndikator']),
                     WriterEntityFactory::createCell('Kab. Bintan'),
                     WriterEntityFactory::createCell(\Helper::formatAngka($n['Sasaran_Angka1']) . ' '.$n['Sasaran_Uraian1']),
-                    WriterEntityFactory::createCell(\Helper::formatUang($n['NilaiUsulan1']))
+                    WriterEntityFactory::createCell(\Helper::formatAngka($n['NilaiUsulan1']))
                 ];
-                $multipleRows = [
-                    WriterEntityFactory::createRow($cells)
-                ];
+                $multipleRows[] = WriterEntityFactory::createRow($cells);
             }
             $this->writer->addRows($multipleRows);
 
