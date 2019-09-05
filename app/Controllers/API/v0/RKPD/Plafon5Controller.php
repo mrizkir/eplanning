b33f0728f4c46b63ef3672b12fe93254d724b55b
mrizkir <mr.romdoni@gmail.com>
2019-09-05 23:18:41 +0700

    ubah struktur rkpd

--- app/Controllers/API/v0/RKPD/Plafon5Controller.php
+++ app/Controllers/API/v0/RKPD/Plafon5Controller.php
@@ -27,6 +27,27 @@ class Plafon5Controller extends Controller {
         $ta=\HelperKegiatan::getTahunPerencanaan(true);
     
         $data = \DB::table('v_rkpd')   
+                    ->select (\DB::raw('
+                        "RKPDID" AS transid,
+                        "PrgID",
+                        "Kd_UrusanUnit",
+                        "Kd_BidangUnit",
+                        "OrgCd",
+                        "OrgNm",
+                        "SOrgCd",
+                        "SOrgNm",
+                        "Kd_Urusan" AS "Kd_UrusanPrg",
+                        "Kd_Bidang" AS "Kd_BidangPrg",
+                        "Kd_Prog",
+                        "PrgNm",
+                        "Kd_Keg",
+                        "KgtNm",
+                        "NilaiUsulan2" AS "Jumlah1",
+                        "Descr",
+                        "TA",
+                        "Status" AS "status",
+                        "EntryLvl"
+                    '))
                     ->where('TA',$ta)
                     ->where('EntryLvl',2)
                     ->get();
