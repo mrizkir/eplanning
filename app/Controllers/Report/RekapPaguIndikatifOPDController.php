3654a2d28c232df761631abeb2a45b45b4d6648a
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-01-17 05:44:22 +0700

    tambah auth

--- app/Controllers/Report/RekapPaguIndikatifOPDController.php
+++ app/Controllers/Report/RekapPaguIndikatifOPDController.php
@@ -15,7 +15,7 @@ class RekapPaguIndikatifOPDController extends Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->middleware(['auth']);
+        $this->middleware(['auth','role:superadmin|bapelitbang|tapd|opd']);
     }
     /**
      * collect data from resources for index view
