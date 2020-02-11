e3387962197782cb83479ad13695056d7577056a
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-02-11 07:14:51 +0700

    info sub kegiatan

--- resources/views/pages/limitless/dmaster/subkegiatan/info.blade.php
+++ resources/views/pages/limitless/dmaster/subkegiatan/info.blade.php
@@ -1,3 +1,3 @@
 <small class="display-block">          
-    Modul ini digunakan untuk melakukan pengolahan data terhadap kegiatan. Kegiatan dikelompokan berdasarkan program.    
+    Modul ini digunakan untuk melakukan pengolahan data terhadap sub kegiatan sesuai PERMENDAGRI No. 90 Tahun 2019. Sub Kegiatan dikelompokan berdasarkan program.    
 </small>
